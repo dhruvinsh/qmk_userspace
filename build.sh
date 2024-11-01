2cb3bbce95b31596162be01797fce89b52aaf366
Dhruvin Shah <dhruvin3@gmail.com>
2024-10-31 22:54:48 -0400

    feat(build): add qmk installation check and parallel build
    - Ensure `qmk` is installed before proceeding with the build.
    - Add parallel build for all custom keyboards using `qmk userspace-compile`.

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@
 for keyboard in ./custom/*; do
     rsync -av --delete "$keyboard" ~/qmk_firmware/keyboards/
 done
+
+# make sure `qmk` is installed
+[ -x "$(command -v qmk)" ] || exit 1
+
+# build all the custom keyboard
+qmk userspace-compile --parallel "$(nproc)" --clean --no-temp
