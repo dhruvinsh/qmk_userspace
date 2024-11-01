767799728708dd90f9a62738f179ac4419d9ef6e
Dhruvin Shah <dhruvin3@gmail.com>
2024-10-31 23:06:24 -0400

    feat(helios): VIAL unlock combo and remove insecure flag
    - Added VIAL_UNLOCK_COMBO_ROWS and VIAL_UNLOCK_COMBO_COLS definitions to config.h
    - Removed VIAL_INSECURE flag from rules.mk

--- keyboards/helios/keymaps/dhruvinsh/config.h
+++ keyboards/helios/keymaps/dhruvinsh/config.h
@@ -2,5 +2,8 @@
 
 #define VIAL_KEYBOARD_UID {0x3D, 0x36, 0x94, 0xF9, 0x71, 0x35, 0x9D, 0x7E}
 
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 220
