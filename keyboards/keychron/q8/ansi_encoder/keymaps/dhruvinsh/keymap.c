68a47de4255e436fb2c0813fe02a0708a43a6629
Dhruvin Shah <dhruvin3@gmail.com>
2024-11-04 22:55:51 -0500

    feat(keymap): implement Colemak-DH with home row mods
    - Add home row mod-tap definitions for GACS (GUI, ALT, CTRL, SHIFT)
    - Remap base layer to Colemak-DH layout
    - Integrate home row mods in the base layer

--- keyboards/keychron/q8/ansi_encoder/keymaps/dhruvinsh/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/dhruvinsh/keymap.c
@@ -27,6 +27,19 @@ enum layers{
     _FN3
 };
 
+
+// HOME ROW MODS
+#define HOME_A LGUI_T(KC_A)
+#define HOME_R LALT_T(KC_R)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_T LSFT_T(KC_T)
+// Right-hand home row mods
+#define HOME_N RSFT_T(KC_N)
+#define HOME_E RCTL_T(KC_E)
+#define HOME_I LALT_T(KC_I)
+#define HOME_O RGUI_T(KC_O)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,             KC_MUTE,
@@ -37,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,             KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_DEL,
-        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,              KC_HOME,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,    KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_DEL,
+        KC_CAPS, HOME_A,   HOME_R,   HOME_S,  HOME_T,  KC_G,              KC_M,     HOME_N,  HOME_E,  HOME_I,   HOME_O,   KC_QUOT,  KC_ENT,              KC_HOME,
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,     XXXXXXX,  KC_K,    KC_H,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(_FN2), MO(_FN3),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN,    KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_69(
