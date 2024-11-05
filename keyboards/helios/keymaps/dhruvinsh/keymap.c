a2e4fc8df2808776ea72604b64656c5e0b6fff58
Dhruvin Shah <dshah@evertz.com>
2024-11-05 17:27:58 -0500

    refactor(keymap): swap semicolon with quote in base layer
    Remove redundant comments and swap the position of semicolon and quote
    keys in the base layer of Helios keyboard layout

--- keyboards/helios/keymaps/dhruvinsh/keymap.c
+++ keyboards/helios/keymaps/dhruvinsh/keymap.c
@@ -7,12 +7,10 @@ enum layers{
 };
 
 // HOME ROW MODS
-// Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
 #define HOME_R LALT_T(KC_R)
 #define HOME_S LCTL_T(KC_S)
 #define HOME_T LSFT_T(KC_T)
-// Right-hand home row mods
 #define HOME_N RSFT_T(KC_N)
 #define HOME_E RCTL_T(KC_E)
 #define HOME_I LALT_T(KC_I)
@@ -46,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_kinesis_adv2(
       KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(1),    MO(1),
       KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-      KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                                   KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_BSLS,
+      KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                                   KC_J,     KC_L,     KC_U,     KC_Y,     KC_QUOT,  KC_BSLS,
       KC_CAPS,  HOME_A,   HOME_R,   HOME_S,   HOME_T,   KC_G,                                   KC_M,     HOME_N,   HOME_E,   HOME_I,   HOME_O,   KC_QUOT,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,                                   KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                 KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                    KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
