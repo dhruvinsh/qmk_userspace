69a13fcc918b200f06964a04322a078fc2e003ff
Dhruvin Shah <dshah@evertz.com>
2024-11-05 17:34:30 -0500

    refactor(keymap): replace magic number with layer constant
    Replace hardcoded layer number with _FN constant for better maintainability

--- keyboards/helios/keymaps/dhruvinsh/keymap.c
+++ keyboards/helios/keymaps/dhruvinsh/keymap.c
@@ -42,7 +42,7 @@ const key_override_t *key_overrides[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base layer: Colemak
     [_BASE] = LAYOUT_kinesis_adv2(
-      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(1),    MO(1),
+      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(_FN),    MO(_FN),
       KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
       KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                                   KC_J,     KC_L,     KC_U,     KC_Y,     KC_QUOT,  KC_BSLS,
       KC_CAPS,  HOME_A,   HOME_R,   HOME_S,   HOME_T,   KC_G,                                   KC_M,     HOME_N,   HOME_E,   HOME_I,   HOME_O,   KC_QUOT,
